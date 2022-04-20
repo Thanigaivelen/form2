8b5f5dfc1c7ee1d8a8aeb391c61cb121385bc9aa
Aladdin❄❄ <90086803+Thanigaivelen@users.noreply.github.com>
2022-04-20 04:21:39 -0700

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -22,30 +22,30 @@ const Login = () => {
             <div className='row justify-content-center'>
                 <div className='col-4'>
                     <div className='main'>
-                        <form onSubmit={handleSubmit}>
+                        <form onSubmit={sample}>
                             <div className="form-group">
                                 <label htmlFor="email">Email address</label>
-                                <input type="email" className="form-control" name='email' id="email" onChange={handleChange}></input>
+                                <input type="email" className="form-control" name='email' id="email" onChange={props.handleChange}></input>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="username">Username</label>
-                                <input type="text" className="form-control" name='username' id="username" onChange={handleChange}></input>
+                                <input type="text" className="form-control" name='username' id="username" onChange={props.handleChange}></input>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password">Password</label>
-                                <input type="password" className="form-control" name='password' id="password" onChange={handleChange}></input>
+                                <input type="password" className="form-control" name='password' id="password" onChange={props.handleChange}></input>
                             </div>
                             <div className="form-group">
                                <label htmlFor="phonenumber">phonenumber</label>
-                               <input type="utf-8" className="form-control" name='number' id="phonenumber" onChange={handleChange} max="9999999999"></input>
+                               <input type="utf-8" className="form-control" name='number' id="phonenumber" onChange={props.handleChange} max="9999999999"></input>
                              </div>   
                               <div className="form-group">
                                 <label htmlFor="file">upload your file here</label>
-                                <input type="file" className="form-control" name='file' id="file" onChange={handleChange}></input>
+                                <input type="file" className="form-control" name='file' id="file" onChange={props.handleChange}></input>
                             </div>
                              <div className="form-group">
                                <label htmlFor="comment">comment</label>
-                               <label type="text" className="form-control" name='number' id="phonenumber" onChange={handleChange} ></label>
+                               <label type="text" className="form-control" name='number' id="phonenumber" onChange={props.handleChange} ></label>
                                <textarea
                                input = "text"
                                maxLength={600}
@@ -55,13 +55,16 @@ const Login = () => {
            
 
                          <div class="form-group">
-                           <input type="checkbox" className="form-check-input" name='check box' id="exampleCheck1" onChange={handleChange}></input>
+                           <input type="checkbox" className="form-check-input" name='check box' id="exampleCheck1" onChange={props.handleChange}></input>
                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
                             </div> 
 
                             <div className="btn-container">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary" onclick={validate}>Submit</button>
+
                             </div>
+                                  {emailErr && <p> your email is invalid</p>}
+                                {pwdError && <p>your password is invalid</p>}
                         </form>
                     </div>
                 </div>
